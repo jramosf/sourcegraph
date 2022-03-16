407e2de80e55175d564344b0abbb5bd021ad9fa4
Chris Wendt <chrismwendt@gmail.com>
2022-03-16 16:54:28 -0600

    log pathspecs

--- internal/codeintel/lockfiles/service.go
+++ internal/codeintel/lockfiles/service.go
@@ -4,6 +4,7 @@ import (
 	"archive/zip"
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"path"
 	"strings"
@@ -52,29 +53,14 @@ func (s *Service) StreamDependencies(ctx context.Context, repo api.RepoName, rev
 	}})
 	defer endObservation(1, observation.Args{})
 
-	paths2 := []string{}
 	for _, pathspec := range lockfilePathspecs {
-		paths2 = append(paths2, string(pathspec))
-	}
-
-	paths, err := s.gitSvc.LsFiles(ctx, repo, api.CommitID(rev), paths2...)
-	if err != nil {
-		return err
-	}
-
-	if len(paths) == 0 {
-		return nil
-	}
-
-	paths3 := []gitserver.Pathspec{}
-	for _, path := range paths {
-		paths3 = append(paths3, gitserver.Pathspec(path))
+		fmt.Println("pathspec", pathspec)
 	}
 
 	opts := gitserver.ArchiveOptions{
 		Treeish:   rev,
 		Format:    "zip",
-		Pathspecs: paths3,
+		Pathspecs: lockfilePathspecs,
 	}
 
 	rc, err := s.gitSvc.Archive(ctx, repo, opts)
