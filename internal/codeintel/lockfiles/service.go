f1e05d6904994eb516e37d2be0c3287505d8b67d
Chris Wendt <chrismwendt@gmail.com>
2022-03-16 16:31:22 -0600

    try bringing back LsFiles

--- internal/codeintel/lockfiles/service.go
+++ internal/codeintel/lockfiles/service.go
@@ -52,10 +52,29 @@ func (s *Service) StreamDependencies(ctx context.Context, repo api.RepoName, rev
 	}})
 	defer endObservation(1, observation.Args{})
 
+	paths2 := []string{}
+	for _, pathspec := range lockfilePathspecs {
+		paths2 = append(paths2, string(pathspec))
+	}
+
+	paths, err := s.gitSvc.LsFiles(ctx, repo, api.CommitID(rev), paths2...)
+	if err != nil {
+		return err
+	}
+
+	if len(paths) == 0 {
+		return nil
+	}
+
+	paths3 := []gitserver.Pathspec{}
+	for _, path := range paths {
+		paths3 = append(paths3, gitserver.Pathspec(path))
+	}
+
 	opts := gitserver.ArchiveOptions{
 		Treeish:   rev,
 		Format:    "zip",
-		Pathspecs: lockfilePathspecs,
+		Pathspecs: paths3,
 	}
 
 	rc, err := s.gitSvc.Archive(ctx, repo, opts)
